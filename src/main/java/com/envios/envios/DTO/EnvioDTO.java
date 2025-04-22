10fc695b71b024bfa937a94a24311c84822ddf2e
GonzaloCaro <gcaro@we-techs.com>
2025-04-21 22:34:40 -0400

    add EnvioDTO with validation constraints for origenId and destinoId

---
+++ src/main/java/com/envios/envios/DTO/EnvioDTO.java
@@ -0,0 +1,38 @@
+package com.envios.envios.DTO;
+
+import jakarta.validation.constraints.*;
+
+public class EnvioDTO {
+    @NotNull(message = "El ID del aeropuerto origen es obligatorio")
+    private Long origenId;
+
+    @NotNull(message = "El ID del aeropuerto destino es obligatorio")
+    private Long destinoId;
+
+    private String descripcion;
+
+    // Getters y Setters
+    public Long getOrigenId() {
+        return origenId;
+    }
+
+    public void setOrigenId(Long origenId) {
+        this.origenId = origenId;
+    }
+
+    public Long getDestinoId() {
+        return destinoId;
+    }
+
+    public void setDestinoId(Long destinoId) {
+        this.destinoId = destinoId;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
